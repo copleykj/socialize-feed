88cf591fda6a15c27f2192a08a414ddcd00e0940
Kelly Copley <copleykj@gmail.com>
2017-08-10 08:22:59 -0400

    add user-blocking to deps

--- package.js
+++ package.js
@@ -2,14 +2,16 @@
 Package.describe({
     name: 'socialize:feed',
     summary: 'A package for impmementing a social network style news feed',
-    version: '0.2.3',
+    version: '1.0.0',
 });
 
 Package.onUse(function _(api) {
     api.versionsFrom('1.3');
 
     api.use([
-        'socialize:user-model@1.0.0', 'socialize:postable@1.0.0',
+        'socialize:user-model@1.0.0',
+        'socialize:user-blocking@1.0.0',
+        'socialize:postable@1.0.0',
     ]);
 
     api.mainModule('server.js', 'server');
