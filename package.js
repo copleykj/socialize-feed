597b8406762c539d044ead9a389099edd885d89c
Kelly Copley <copleykj@gmail.com>
2018-05-26 23:21:19 -0400

    bump version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
     name: 'socialize:feed',
     summary: 'A package for impmementing a social network style news feed',
-    version: '1.0.1',
+    version: '1.0.2',
     git: 'https://github.com/copleykj/socialize-feed.git',
 });
 
