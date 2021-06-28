e80a79634b6af906896f46f451ac4b31181b8ea5
Kelly Copley <copleykj@gmail.com>
2021-06-27 23:49:19 -0400

    allow older versions of meteor and update deps

--- package.js
+++ package.js
@@ -7,16 +7,16 @@ Package.describe({
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom('2.3');
+    api.versionsFrom(['1.10.2', '2.3']);
 
     api.use([
         'check',
         'reywood:publish-composite@1.7.3',
-        'socialize:user-blocking@1.0.5',
-        'socialize:postable@1.0.3',
+        'socialize:user-blocking@1.0.6',
+        'socialize:postable@1.0.4',
     ]);
 
-    api.use('socialize:friendships@1.1.1', { weak: true });
+    api.use('socialize:friendships@1.1.2', { weak: true });
 
     api.mainModule('server/server.js', 'server');
     api.mainModule('common/common.js', 'client');
